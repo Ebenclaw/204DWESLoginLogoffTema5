9f132d57215e544e1518b0fbf165fb0b5f4b983b
Ebenclaw <rebekasanper15@gmail.com>
2023-11-28 10:32:40 +0100

    Version1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# Proyecto LoginLogoffTema5
+## Autor
+Rebeca Sánchez Pérez
+## Requisitos minimos del desarrollo
+- [Apache2.4.52]
+- [MySQL8.0.35]
+- [PHP8.1]
+- [XDebug3.1.2]
+## Descripcion
+Aplicacion Login Logoff correspondiente al ejercicio 3 del tema 5 de la asignatura DWES
+## Funcionalidades
+## Entorno de desarrollo
+[http://daw204.isauces.local/204DWESLoginLogoffTema5/indexLoginLogoffTema5.php](http://daw204.isauces.local/204DWESLoginLogoffTema5/indexLoginLogoffTema5.php)
+## Entorno de explotacion
+[https://daw204.ieslossauces.es/204DWESLoginLogoffTema5/indexLoginLogoffTema5.php](https://daw204.ieslossauces.es/204DWESLoginLogoffTema5/indexLoginLogoffTema5.php)
---
+++ indexLoginLogoffTema5.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webroot/css/style.css">
+    <link rel="icon" type="image/x-icon" href="/webroot/image/flora.png">
+    <title>Rebeca Sánchez Pérez</title>
+</head>
+
+<body>
+    <header>
+        <p id="pTitulo">Bienvenido a mi aplicacion</p>
+        <h1>Login Logoff</h1>
+    </header>
+    <main>         
+        <div class="container">
+            <ul>
+                <li><a href="codigoPHP/Login.php">Iniciar sesion</a></li>
+            </ul>
+            <img src="webroot/image/esquema.PNG" alt="alt"/>
+        </div>
+    </main>
+    <footer>
+        <div id="derechos">2023-2024 © Todos los derechos reservados: <a href="../index.html">Rebeca Sánchez Pérez</a></div>
+        <div id="fotos">
+            <a href="https://github.com/Ebenclaw/204DWESProyectoTema5" target="_blank"><img id="git" src="webroot/image/GitHub.png" alt="GitHub"></a>
+            <a href="http://ieslossauces.centros.educa.jcyl.es/sitio/" target="_blank"><img id="sauces" src="webroot/image/sauces.png" alt="Sauces"></a>
+            <a href="../204DWESProyectoDWES/indexProyectoDWES.php"><img id="home" src="webroot/image/home.png" alt="Inicio"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
+
+
+
